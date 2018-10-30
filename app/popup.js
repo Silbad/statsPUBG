33be1c06d618e071e998840bbac21f937f5d074a
silbad <psilbad@gmail.com>
2018-10-30 23:09:33 +0100

    Update design, add errors and reduce number of requests

--- app/popup.js
+++ app/popup.js
@@ -1,8 +1,65 @@
-var apiKey = 'YOUR_API_KEY';
+'use strict';
+var apiKey = config.API_KEY;
 
+// init statsPUBG
 $(function() {
     initStatsPUBG();
 });
+
+// charger une erreur
+function loadError(error) {
+    $('.dataPUBG').text('n/a');
+    $('.loading').hide();
+    clearTimeout();
+    switch(error) {
+        case 'Not Found':
+            error = 'Data not found, please change parameters.'
+            break;
+        case 'No Reason Phrase':
+            error = 'Too many requests, please wait a minute.'
+            break;
+        default:
+            error = 'No data available.'
+    }
+    $('.too-requests').html('<i class="fa fa-exclamation-triangle"></i> ' + error).show();
+    var tmpError = setTimeout(function(){
+        $('.too-requests').hide();
+    }, 5000);
+}
+
+// mettre à jour la liste des saisons
+function updateListSeasons(tmpPubgServer) {
+    $('.loading').show();
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.playbattlegrounds.com/shards/' + tmpPubgServer + '/seasons',
+        headers: {
+            Accept: 'application/vnd.api+json'
+        },
+        beforeSend: function (auth) {
+            auth.setRequestHeader('Authorization', 'Bearer ' + apiKey);
+        },
+        success: function(result, statut, xhr) {
+            // charger et enregistrer la liste des saisons
+            browser.storage.local.set({ pubgListSeasons: result });
+            var listSessions = '';
+            $.each(result.data.reverse(), function(key, value) {
+                listSessions += '<option value="' + value.id + '">' + value.id.replace('division.bro.official.', '') + '</option>';
+            });
+            $('#pubg-season').empty().append(listSessions);
+            // sauvegarder la saison par défaut
+            var tmpPubgSeason = $('#pubg-season').val();
+            browser.storage.local.set({ pubgSeason: tmpPubgSeason });
+            // sauvegarder la date de mise à jour
+            var tmpSaisonUpdate = new Date();
+            browser.storage.local.set({ pubgSaisonUpdate: tmpSaisonUpdate });
+        },
+        error: function(xhr, statut, error){
+            loadError(error);
+        }
+    });
+    $('.loading').hide();
+}
 
 // initialiser l'application
 function initStatsPUBG() {
@@ -11,52 +68,52 @@ function initStatsPUBG() {
     var manifest = chrome.runtime.getManifest();
     $('.version').html(manifest.version);
 
-    browser.storage.local.get(['pubgServer', 'pubgListSeasons', 'pubgName', 'pubgSeason', 'pubgMode']).then(function(item) {
+    // récupérer les données
+    browser.storage.local.get(['pubgName', 'pubgServer', 'pubgSaisonUpdate', 'pubgListSeasons', 'pubgSeason', 'pubgMode']).then(function(item) {
 
-        // sélection du serveur
-        if (item.pubgServer != undefined) {
-            $('#pubg-server').val(item.pubgServer);
-        }
+        // récupérer l'ID du serveur
         var tmpPubgServer = $('#pubg-server').val();
+        if (item.pubgServer != null) {
+            tmpPubgServer = item.pubgServer
+        }
         browser.storage.local.set({ pubgServer: tmpPubgServer });
 
-        // liste des saisons
-        var listSessions = '';
-        if (item.pubgListSeasons != undefined) {
-            $.each(item.pubgListSeasons.data.reverse(), function(key, value) {
-                listSessions += '<option value="' + value.id + '">' + value.id.replace('division.bro.official.', '') + '</option>';
-            });
-            $('#pubg-season').empty().append(listSessions);
-        } else {
-            $('.loading').show();
+        // mettre à jour la liste des saisons une fois par mois
+        if (item.pubgListSeasons != null) {
+            // vérifier la cohérence d'une mise à jour de la liste des saisons
+            var tmpPubgServerUpdate = item.pubgSaisonUpdate;
+            if (tmpPubgServerUpdate == null) {
+                tmpPubgServerUpdate = new Date();
+            }
+            var tmpNow = new Date();
+            var tmpDay = tmpNow.getDate();
+            var timeDiff = Math.abs(tmpNow.getTime() - tmpPubgServerUpdate.getTime());
+            var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
-            $.ajax({
-                method: 'GET',
-                url: 'https://api.playbattlegrounds.com/shards/' + tmpPubgServer + '/seasons',
-                headers: {
-                    Accept: 'application/vnd.api+json'
-                },
-                beforeSend: function (auth) {
-                    auth.setRequestHeader('Authorization', 'Bearer ' + apiKey);
-                },
-                success: function(result, statut, xhr) {
-                    browser.storage.local.set({ pubgListSeasons: result });
-                    $.each(result.data.reverse(), function(key, value) {
+            // si la date est comprise entre le 1 et le 5
+            if ((tmpDay > 0) && (tmpDay < 6) && (diffDays > 5)) {
+                updateListSeasons(tmpPubgServer);
+            } else {
+                // si mise à jour vieille de plus de 28 jours, on met à jour
+                if (diffDays > 15) {
+                    updateListSeasons(tmpPubgServer);
+                // si liste à jour on la charge normalement
+                } else {
+                    var listSessions = item.pubgListSeasons;
+                    $.each(listSessions.data.reverse(), function(key, value) {
                         listSessions += '<option value="' + value.id + '">' + value.id.replace('division.bro.official.', '') + '</option>';
                     });
                     $('#pubg-season').empty().append(listSessions);
-                    $('.loading').hide();
-                },
-                error: function(xhr, statut, error){
-                    $('.dataPUBG').text('n/a');
-                    $('.loading').hide();
                 }
-            });
+            }
+        } else {
+            updateListSeasons(tmpPubgServer);
         }
 
         // récupérer les données stockées, sinon prendre celles par défaut et les stocker
         if (item.pubgName != undefined) {
             $('#pubg-name').val(item.pubgName);
+            $('#pubg-server').val(item.pubgServer);
             $('#pubg-season').val(item.pubgSeason);
             $('#pubg-mode').val(item.pubgMode);
 
@@ -65,6 +122,8 @@ function initStatsPUBG() {
         } else {
             var tmpPubgName = $('#pubg-name').val();
             browser.storage.local.set({ pubgName: tmpPubgName });
+            var tmpPubgServer = $('#pubg-server').val();
+            browser.storage.local.set({ pubgServer: tmpPubgServer });
             var tmpPubgSeason = $('#pubg-season').val();
             browser.storage.local.set({ pubgSeason: tmpPubgSeason });
             var tmpPubgMode = $('#pubg-mode').val();
@@ -79,48 +138,19 @@ function initStatsPUBG() {
             updateStatsPUBG();
         });
 
-        // sur modification server
-        $('#pubg-server').on('change', function(e) {
-
-            $('.loading').show();
-
-            // mettre à jour la liste des saisons
-            var tmpPubgServer = $(this).val();
-            browser.storage.local.set({ pubgServer: tmpPubgServer });
-            $.ajax({
-                method: 'GET',
-                url: 'https://api.playbattlegrounds.com/shards/' + tmpPubgServer + '/seasons',
-                headers: {
-                    Accept: 'application/vnd.api+json'
-                },
-                beforeSend: function (auth) {
-                    auth.setRequestHeader('Authorization', 'Bearer ' + apiKey);
-                },
-                success: function(result, statut, xhr) {
-                    browser.storage.local.set({ pubgListSeasons: result });
-                    var listSessions = '';
-                    $.each(result.data.reverse(), function(key, value) {
-                        listSessions += '<option value="' + value.id + '">' + value.id.replace('division.bro.official.', '') + '</option>';
-                    });
-                    $('#pubg-season').empty().append(listSessions);
-                    var tmpPubgSeason = $('#pubg-season').val();
-                    browser.storage.local.set({ pubgSeason: tmpPubgSeason });
-                },
-                error: function(xhr, statut, error){
-                    $('.dataPUBG').text('n/a');
-                    $('.loading').hide();
-                }
-            });
-
-            $('.loading').hide();
-
-            updateStatsPUBG();
-        });
-
         // sur modification nom
         $('#pubg-name').on('blur change', function(e) {
             var tmpPubgName = $(this).val();
             browser.storage.local.set({ pubgName: tmpPubgName });
+            updateStatsPUBG();
+        });
+
+        // sur modification server
+        $('#pubg-server').on('change', function(e) {
+            var tmpPubgServer = $(this).val();
+            browser.storage.local.set({ pubgServer: tmpPubgServer });
+            updateListSeasons(tmpPubgServer);
+            updateStatsPUBG();
         });
 
         // sur modification saison
@@ -154,8 +184,10 @@ function updateStatsPUBG() {
 
     // tester si le nom est saisi
     if((tmpPubgName.trim() != '') && (tmpPubgServer.trim() != '') && (tmpPubgSeason.trim() != '') && (tmpPubgMode.trim() != '')) {
+
         $.ajax({
             method: 'GET',
+            async: false,
             url: 'https://api.playbattlegrounds.com/shards/' + tmpPubgServer + '/players?filter[playerNames]=' + tmpPubgName,
             headers: {
                 Accept: 'application/vnd.api+json'
@@ -164,11 +196,11 @@ function updateStatsPUBG() {
                 auth.setRequestHeader('Authorization', 'Bearer ' + apiKey);
             },
             success: function(result, statut, xhr) {
-
                 var tmpPubgId = result.data[0].id;
 
                 $.ajax({
                     method: 'GET',
+                    async: false,
                     url: 'https://api.playbattlegrounds.com/shards/' + tmpPubgServer + '/players/' + tmpPubgId + '/seasons/' + tmpPubgSeason,
                     headers: {
                         Accept: 'application/vnd.api+json'
@@ -185,56 +217,62 @@ function updateStatsPUBG() {
                                 $('.boosts').text(value.boosts);
                                 $('.dBNOs').text(value.dBNOs);
                                 $('.dailyKills').text(value.dailyKills);
-                                $('.damageDealt').text(value.damageDealt);
+                                $('.damageDealt').text(parseInt(value.damageDealt));
                                 $('.days').text(value.days);
                                 $('.headshotKills').text(value.headshotKills);
                                 $('.heals').text(value.heals);
                                 $('.killPoints').text(value.killPoints);
                                 $('.kills').text(value.kills);
-                                $('.longestKill').text(value.longestKill);
-                                $('.longestTimeSurvived').text(value.longestTimeSurvived);
+                                var tmpLongestKill = parseFloat(value.longestKill).toFixed(2);
+                                $('.longestKill').text(tmpLongestKill + ' m');
+                                var tmpLongestTimeSurvived = parseInt(value.longestTimeSurvived / 60);
+                                $('.longestTimeSurvived').text(tmpLongestTimeSurvived + ' min');
                                 $('.losses').text(value.losses);
                                 $('.maxKillStreaks').text(value.maxKillStreaks);
-                                $('.mostSurvivalTime').text(value.mostSurvivalTime);
+                                var tmpMostSurvivalTime = parseInt(value.mostSurvivalTime / 60);
+                                $('.mostSurvivalTime').text(tmpMostSurvivalTime + ' min');
                                 $('.revives').text(value.revives);
-                                $('.rideDistance').text(value.rideDistance);
+                                var tmpRideDistance = parseFloat(value.rideDistance / 1000).toFixed(2);
+                                $('.rideDistance').text(tmpRideDistance + ' km');
                                 $('.roadKills').text(value.roadKills);
                                 $('.roundMostKills').text(value.roundMostKills);
                                 $('.roundsPlayed').text(value.roundsPlayed);
                                 $('.suicides').text(value.suicides);
                                 $('.teamKills').text(value.teamKills);
-                                $('.timeSurvived').text(value.timeSurvived);
+                                var tmpTimeSurvived = parseInt(value.timeSurvived / 60);
+                                $('.timeSurvived').text(tmpTimeSurvived + ' min');
                                 $('.top10s').text(value.top10s);
                                 $('.vehicleDestroys').text(value.vehicleDestroys);
-                                $('.walkDistance').text(value.walkDistance);
+                                var tmpWalkDistance = parseFloat(value.walkDistance / 1000).toFixed(2);
+                                $('.walkDistance').text(tmpWalkDistance + ' km');
                                 $('.weaponsAcquired').text(value.weaponsAcquired);
                                 $('.weeklyKills').text(value.weeklyKills);
                                 $('.winPoints').text(value.winPoints);
                                 $('.wins').text(value.wins);
+                                $('.bestRankPoint').text(value.bestRankPoint);
+                                $('.dailyWins').text(value.dailyWins);
+                                $('.rankPoints').text(value.rankPoints);
+                                var tmpSwimDistance = parseFloat(value.swimDistance / 1000).toFixed(2);
+                                $('.swimDistance').text(tmpSwimDistance + ' km');
 
                                 isData = true;
                                 $('.loading').hide();
                             }
                         });
                         if (isData == false) {
-                            $('.dataPUBG').text('n/a');
-                            $('.loading').hide();
+                            loadError();
                         }
                     },
                     error: function(xhr, statut, error){
-                        $('.dataPUBG').text('n/a');
-                        $('.loading').hide();
+                        loadError(error);
                     }
                 });
-
             },
             error: function(xhr, statut, error){
-                $('.dataPUBG').text('n/a');
-                $('.loading').hide();
+                loadError(error);
             }
         });
     } else {
-        $('.dataPUBG').text('n/a');
-        $('.loading').hide();
+        loadError();
     }
 }
